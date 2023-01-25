bc3a87e4f811ea16d5f7180b33938279674c317e
Moxos7887 <68481244+Moxos7887@users.noreply.github.com>
2023-01-25 17:29:55 -0400

    Initial commit

---
+++ src/Coche.java
@@ -0,0 +1,9 @@
+public class Coche {
+    public int numero_de_puertas;
+    public Coche(int numero_de_puertas) {
+        this.numero_de_puertas = numero_de_puertas;
+    }
+    public void add_num_puertas(int numero_de_puertas) {
+       this.numero_de_puertas+=numero_de_puertas;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("suma de numeros: "+ suma(2,5,6));
+        Coche toyota=new Coche(1);
+        System.out.println("numero de puertas: "+toyota.numero_de_puertas);
+    }
+    public static int suma(int a, int b, int c)
+    {
+        return a+b+c;
+    }
+}
